02411e6d811ef2e40b15fb72a0f8c8ded6521ac6
TropheusJ <jverrellijr@icloud.com>
2026-05-01 20:05:29 -0400

    remove dummy MC class explodes in prod

--- test/mod/src/main/java/fish/cichlidmc/test_mod/TestPreLaunch.java
+++ test/mod/src/main/java/fish/cichlidmc/test_mod/TestPreLaunch.java
@@ -2,13 +2,12 @@ package fish.cichlidmc.test_mod;
 
 import fish.cichlidmc.cichlid.api.loaded.Mod;
 import fish.cichlidmc.cichlid.api.mod.entrypoint.PreLaunchEntrypoint;
-import net.minecraft.Dummy;
 
 public class TestPreLaunch implements PreLaunchEntrypoint {
 	@Override
 	public void preLaunch(Mod mod) {
 		System.out.println("Test mod pre-launch!!!!");
 		// this should succeed
-		Dummy.load();
+		//Dummy.load();
 	}
 }
